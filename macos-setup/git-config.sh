0ce91fb8e52e6fe83d5992cbafcb6e1ab1036931
sam <shaoming.mei@gmail.com>
2020-09-29 14:32:15 +0800

    Create git-config.sh

---
+++ macos-setup/git-config.sh
@@ -0,0 +1,7 @@
+git config --global user.name meishaoming
+git config --global user.email meishaoming@foxmail.com
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global core.editor vim
