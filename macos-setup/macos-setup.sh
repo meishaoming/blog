a221872fa6557f91928dd041154275efc220c425
sam <shaoming.mei@gmail.com>
2021-10-11 01:40:22 +0800

    Update macos-setup.sh

--- macos-setup/macos-setup.sh
+++ macos-setup/macos-setup.sh
@@ -9,10 +9,6 @@
 brew update
 brew install tmux macvim the_silver_searcher imagemagick
 
-# oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-compaudit | xargs chmod g-w,o-w
-
 # vim
 rm -rf ~/.vim
 git clone --recursive https://github.com/meishaoming/oh-my-vim.git ~/.vim
@@ -28,10 +24,15 @@ cp -rf ~/rime-temp/* ~/Library/Rime/
 rm -rf ~/rime-temp
 
 # wireshark
-brew cask install wireshark
+#brew cask install wireshark
 
 # git
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/meishaoming/blog/master/macos-setup/git-config.sh)"
 
 # adb
 brew cask install android-platform-tools
+
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+compaudit | xargs chmod g-w,o-w
